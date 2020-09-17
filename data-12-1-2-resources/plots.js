a91dd94096330e014d8081435b1f474b983fad1b
Mac <mac@MacdeMacBook-Pro.local>
2020-09-17 11:26:56 -0400

    c

---
+++ data-12-1-2-resources/plots.js
@@ -0,0 +1,32 @@
+Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+
+var trace = {
+    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: "bar"
+   };
+var data = [trace];
+var layout = {
+    title: "'Bar' Chart",
+    xaxis: { title: "Drinks"},
+    yaxis: { title: "% of Drinks Ordered"}
+};
+
+var trace = {
+    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: 'pie'
+   };
+var data = [trace];
+var layout = {
+    title: "'Bar' Chart",
+};
+
+var numbers = [1,2,3,4,5];
+var doubled = numbers.map(function(num){
+    return num * 2;
+});
+console.log(doubled);
+
+Plotly.newPlot("plotArea", data, layout);
